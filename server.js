4e6e41d84ba52d8e3e4cb68134fcb3682508c1bb
DylanMacres <Dylan.Macres@du.edu>
2022-10-29 15:22:23 -0600

    added the server js file to connect

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
-DB_NAME='userMusic_db'
-DB_USER='root'
-DB_PASSWORD='Cjwq1jhm!'
+const session = require("express-session");
+const express = require("express");
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+
+
+const sesssssion = {
+    secret: 'Keep my data safe duh',
+    cookie: {
+        maxAge: 420000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sesssssion));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/'));
+const hbs = exphbs.create({
+    helpers
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
+  });
+  
