6b534d70e38c0e9fd0431e5fe829b42a7afb54f8
DylanMacres <Dylan.Macres@du.edu>
2022-10-30 17:35:17 -0600

    adding api posts and stuff

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,34 @@
+const { Post } = require('../../models/');
+
+const withAuthorization = require('../../utils/authorization');
+
+const router = require('express').Router();
+
+
+router.post('/', withAuthorization, async (req, res) => {
+    const body = req.body;
+    try{
+        const post = await Post.create({ ...body, userId: req.session.userId });
+        res.json(post);
+    }catch(err){
+        res.status(500).json(err)
+    }
+});
+
+
+router.put('/:id', withAuthorization, async (req,res) => {
+    try{
+        const [affectedStuff] = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if(affectedStuff > 0){
+            res.status(200).end();
+        }else {
+            res.status(404).end();
+        }
+    }catch(err) {
+        res.status(500).json(err);
+    }
+} )
