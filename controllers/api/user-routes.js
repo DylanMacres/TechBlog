84d6ed14b8dc7084b57e76369083ac56d70b41cd
DylanMacres <Dylan.Macres@du.edu>
2022-10-30 18:40:44 -0600

    adding api users and stuff

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -34,6 +34,23 @@ router.post('/login', async (req,res ) => {
             res.status(400).json({ message: "Not the droids you're looking for" });
             return;
         }
-        
+        const validPass = user.checkPassword(req.body.password);
+        if(!validPass) {
+            res.status(400).json({message: "Couldn't find that account!" });
+            return;
+        }
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.username = user.username;
+            req.session.loggedIn = true;
+
+            res.json({ user, message:"good job, you're now in the matrix!"});
+        });
+    }catch(err) {
+        res.status(404).json({
+            message: "Password or Username not found!"
+        });
     }
-})
+});
+
+
